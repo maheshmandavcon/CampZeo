5148254251b6464d2a943050fb43f40a81e3ed6a
Mahesh <67687396+maheshmandavcon@users.noreply.github.com>
2026-04-10 11:42:34 +0530

    middle ware code changes

--- middleware.ts
+++ middleware.ts
@@ -115,7 +115,7 @@ export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
+        // Run for API routes, EXCEPT for the Google Drive media proxy (to avoid Clerk headers)
+        '/((?!api/upload/google-drive/view)(api|trpc)(.*))',
     ],
 };
